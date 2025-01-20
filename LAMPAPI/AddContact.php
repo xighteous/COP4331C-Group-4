7213b449d222a8907617ea8d3b405fbfbdf0603d
DilanIsHere <dhamidworks13@gmail.com>
2025-01-19 19:27:01 -0500

    Add several PHP files to LAMPAPI
    Uploaded .php files [Login, Register, AddContact, SearchContact, DeleteContact]. Login.php unchanged from example. All files untested.

---
+++ LAMPAPI/AddContact.php
@@ -0,0 +1,44 @@
+<?php
+
+    $inData = getRequestInfo();
+
+    $fName = $inData["fname"];
+    $lName = $inData["lname"];
+    $number = $inData["number"];
+    $email = $inData["email"];
+    $userId = $inData["userId"];
+
+    // Possibly needs change for username + password
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+    if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Contacts (UserId,FirstName,LastName,Phone,Email) VALUES(?,?,?,?,?)");
+		$stmt->bind_param("sssss", $userId, $fName, $lName, $number, $email);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+    function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
---
+++ LAMPAPI/DeleteContact.php
@@ -0,0 +1,29 @@
+<?php
+
+    $inData = getRequestInfo();
+
+    $fName = $inData["fName"];
+    $userId = $InData["userId"];
+
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("DELETE FROM Contacts WHERE FirstName=? AND UserId=?");
+	}
+
+    function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+    function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
