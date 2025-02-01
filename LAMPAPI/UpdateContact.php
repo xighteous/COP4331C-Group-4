dc9b1e309b597b34b2ee51124af57bdf6731805e
DilanIsHere <dhamidworks13@gmail.com>
2025-01-31 23:24:06 -0500

    Update UpdateContact.php

--- LAMPAPI/UpdateContact.php
+++ LAMPAPI/UpdateContact.php
@@ -1,40 +1,49 @@
 <?php
 
-    $inData = getRequestInfo();
+	/*
+		Updates Contact info with new values
+  		Input: {firstName,lastName,phoneNumber,emailAddress,ID}
+    		Output: {error}
+	*/
 
-    $fName = $inData["firstName"];
-    $lName = $inData["lastName"];
-    $number = $inData["phoneNumber"];
-    $email = $inData["emailAddress"];
-    $userId = $inData["userId"];
-	$ID = $inData["ID"];
+	$inData = getRequestInfo();
 
-    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-    if ($conn->connect_error) 
+    	$firstName = $inData["firstName"];
+    	$lastName = $inData["lastName"];
+    	$phoneNumber = $inData["phoneNumber"];
+    	$emailAddress = $inData["emailAddress"];
+    	$ID = $inData["ID"];
+
+	// Main code block
+    	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+    	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
 	} 
 	else
 	{
-		$stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, Phone=?, Email=? WHERE UserID=? AND ID=?");
-		$stmt->bind_param("ssssss", $fName, $lName, $number, $email, $userId, $ID);
+		$stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, Phone=?, Email=? WHERE ID=?");
+		$stmt->bind_param("sssssi", $firstName, $lastName, $phoneNumber, $emailAddress, $ID);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
 		returnWithError("");
 	}
 
-    function getRequestInfo()
+	// Fetches input data
+    	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
+	// Sends output
 	function sendResultInfoAsJson( $obj )
 	{
 		header('Content-type: application/json');
 		echo $obj;
 	}
-	
+
+	// Returns $err as error JSON
 	function returnWithError( $err )
 	{
 		$retValue = '{"error":"' . $err . '"}';
