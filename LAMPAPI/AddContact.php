69b4f4d248a10d71fa17f4a72850d7ea7469687f
DilanIsHere <dhamidworks13@gmail.com>
2025-01-31 22:10:19 -0500

    Update AddContact.php

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -1,15 +1,22 @@
 <?php
 
-    $inData = getRequestInfo();
+	/*
+ 		Adds new contact to the database
+   		Input: {firstName,lastName,phoneNumber,emailAddress,userId}
+     		Output: {error}
+	*/
 
-    $fName = $inData["firstName"];
-    $lName = $inData["lastName"];
-    $number = $inData["phoneNumber"];
-    $email = $inData["emailAddress"];
-    $userId = $inData["userId"];
+    	$inData = getRequestInfo();
 
-    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-    if ($conn->connect_error) 
+    	$fName = $inData["firstName"];
+    	$lName = $inData["lastName"];
+    	$number = $inData["phoneNumber"];
+    	$email = $inData["emailAddress"];
+    	$userId = $inData["userId"];
+
+	// Main code block
+    	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+    	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
 	} 
@@ -23,17 +30,20 @@
 		returnWithError("");
 	}
 
-    function getRequestInfo()
+	// Fetches input data
+    	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
+	// Sends output
 	function sendResultInfoAsJson( $obj )
 	{
 		header('Content-type: application/json');
 		echo $obj;
 	}
-	
+
+	// Returns $err as error JSON
 	function returnWithError( $err )
 	{
 		$retValue = '{"error":"' . $err . '"}';
